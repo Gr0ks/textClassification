59bebb798505ff02478baa9f23e20445e7c0b7a7
Илья Петренко <gr0x562@gmail.com>
2020-07-06 23:57:17 +0300

    remove not used code

--- index.js
+++ index.js
@@ -65,34 +65,3 @@ app.post('/api/createClassifier', async (req, res) => {
 app.listen(9980, () => {
   console.log('Server has been started on port 9980...')
 })
-
-
-
-
-// app.get('/api/rooms', (req, res) => {
-//   res.status(200).json(ROOMS.map(r =>({id: r.id, name: r.name})))
-// });
-
-// app.get('/api/room/:id', (req, res) => {
-//   const {script, returns} = ROOMS.find(r => (+r.id) === (+req.params.id));
-//   res.status(200).json({script, returns})
-// });
-
-// app.get('/api/runScript/:id', (req, res) => {
-//   const room = ROOMS.find(r => (+r.id) === (+req.params.id));
-//   room.returns = eval(room.script);
-//   const {script, returns} = room;
-//   res.status(200).json({script, returns});
-// });
-
-// app.put('/api/room/:id', (req, res) => {
-//   const room = ROOMS.find(r => (+r.id) === (+req.params.id));
-//   room.script = req.body.script;
-//   res.status(200).json({ok: true});
-// });
-
-// app.use(express.static(path.resolve(__dirname, 'client', 'dist')));
-
-// app.get('*', (req, res) => {
-//   res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'))
-// })
